5107163ec9fd22c055694c6fbcde7153cff98d82
Gagandeep <gagandeep.developer.95@gmail.com>
2021-06-09 10:56:19 -0400

    Finsihed Header Section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Header from './components/header/Header';
+import axios from 'axios';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  useEffect(() => {
+    axios.get(`https://disease.sh/v3/covid-19/all`)
+    .then(res => {
+      console.log(res.data);
+    })
+  }, []);
+
+  useEffect(() => {
+        //  This function will show list of countries data
+    const getCountriesData = () => {
+      axios.get(`https://disease.sh/v3/covid-19/countries`)
+      .then(res => {
+        console.log(res.data);
+        const countries = res.data.map(country => ({
+            name : country.country,
+            value : country.countryInfo.iso2,
+        }))
+        setCountries(countries);
+      })
+      .catch(error => {
+        console.log(error);
+      })    
+    }
+    getCountriesData();
+  }, []);
+
+// This function will select the specific country and changes info accordingly
+
+const onCountryChange = (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header  country={country} countries={countries} onCountryChange={onCountryChange}/>
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import  './Header.css';
+import { FormControl, Select, MenuItem } from '@material-ui/core';
+
+const  Header = ({country,countries, onCountryChange}) =>  {
+    
+    return (
+        <header>
+            <div className="logo">
+                <h1>COVID-19 Tracker</h1>
+            </div>
+            <div className="countries__list">
+                <FormControl className="countries__list__dropdown">
+                    <Select labelId="label" variant="outlined"  id="select" value={country} onChange={onCountryChange}>
+                                    {/* Worldwide data */}
+                    <MenuItem  value="worldwide">Worldwide</MenuItem>
+                        {
+                            countries.map(country => (    
+                                <MenuItem   value={country.value}>{country.name}</MenuItem>
+                            ))
+                        }
+                    </Select>
+                </FormControl>
+            </div>
+        </header>
+     )
+}
+
+export default Header
